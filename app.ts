c4ee8e158a0536db3068aad711ec7775d467ccc2
Axel TERRIER <47625927+axelterrier@users.noreply.github.com>
2023-02-05 22:40:04 +0100

    Ajout fonction basique
    Get/Put sur cocktail et cocktail_composition

--- app.ts
+++ app.ts
@@ -1,19 +1,14 @@
-//#region require
-
 require("dotenv").config();
-
 const cors = require("cors");
-const { exit } = require("process");
-const { default: jwtDecode } = require("jwt-decode");
-const { format } = require('date-fns');
+const jwtDecode = require("jwt-decode");
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
 const auth = require("./middleware/auth");
-const jwt_decode = require("jwt-decode");
-const swaggerJsDoc = require('swagger-jsdoc')
-const swaggerUi = require('swagger-ui-express')
-const url = "/api/V1"
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const mysql = require('mysql');
+
 const swaggerOptions = {
   swaggerDefinition: {
     info: {
@@ -24,104 +19,270 @@ const swaggerOptions = {
       servers: ["https://localhost:8888/api/V1"]
     }
   },
-  // on donne l'endroit ou sont les routes
   apis: ["app.ts"]
 };
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+const url = "/api/V1";
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'cocktail-machine'
+});
 
-app.use(cors({ origin: 'http://localhost:8100', methods: "GET,HEAD,PUT,PATCH,POST,DELETE", allowedHeaders: ['content-type'], credentials: true }));
-const swaggerDocs = swaggerJsDoc(swaggerOptions)
-//#endregion
+app.use(cors({
+  origin: 'http://localhost:8100',
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: ['content-type'],
+  credentials: true
+}));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
 app.use(express.json());
 
-var listener = app.listen(8888, function () {
-  console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+const listener = app.listen(8888, () => {
+  console.log(`Listening on port ${listener.address().port}`);
 });
 
-//#region route
 
-//#region api utilisateur
+///ROUTES
 
-// URL : /api/V1
 
-app.post(url + "/user", async (req, res) => {
-  try {
-    if(req.body == null){
-      const data = 'nullos'
-      console.log(data)
-      res.status(200).json({ message: "Les données de l'utilisateur ont été enregistrées", data });
-    }else{
-      const { given_name, family_name, picture, email } = req.body;
-      const data = { given_name, family_name, picture, email };
-      console.log(data)
-      res.status(200).json({ message: "Les données de l'utilisateur ont été enregistrées", data });
-    }
-    
-    // Insérer les données de l'utilisateur dans la base de données ou faire ce que vous voulez avec elles
-  } catch (err) {
-    res.status(500).send({ message: "Une erreur s'est produite", error: err }).end();
-  }
-});
+app.get(url + '/utilisateurs', async (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err
+    console.log('connected as id ' + connection.threadId)
+    connection.query('SELECT * from utilisateurs', (err, rows) => {
+      connection.release() // return the connection to pool
 
-//#endregion
-
-//#endregion
-
-//#region fonction
-
-//Vérifie qu'un token existe et qu'y a pas d'erreur dans le token
-function checkToken(req, res) {
-  const token = req.cookies['token']
-
-  if (!token) {
-    res.status(403).send("A token is required for authentication").end();
-  } else {
-    var payload
-    try {
-      payload = jwt.verify(token, process.env.TOKEN_KEY)
-    } catch (e) {
-      if (e instanceof jwt.JsonWebTokenError) {
-        return res.status(401).end()
+      if (!err) {
+        res.send(rows)
+      } else {
+        console.log(err)
       }
-      return res.status(400).end()
+
+      // if(err) throw err
+      console.log('Données utilisateurs : \n', rows)
+    })
+  })
+})
+
+app.get(url + '/utilisateurs/:email', async (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      return res.send({
+        error: "Une erreur s'est produite lors de la connexion à la base de données."
+      });
     }
-  }
-  exit
-}
 
-//récupère et décode le token
-function getInfoToken(req, res) {
-  const token = req.cookies['token'];
-  let decodedToken = jwt_decode(token);
-  return decodedToken;
-}
+    let email = req.params.email;
+    let sql = "SELECT * FROM utilisateurs WHERE email = ?";
+    connection.query(sql, email, (err, result) => {
+      connection.release(); // return the connection to pool
 
-//Vérifie l'ID du token et l'ID demandé dans l'URL correspond
-function checkIDToken(req, res) {
-  let decodedToken = JSON.stringify(getInfoToken(req, res).user.id);
-}
+      if (!err) {
+        if (result.length > 0) {
+          res.send({
+            prenom: result[0].prenom,
+            nom: result[0].nom,
+            email: result[0].email,
+            photo_url: result[0].photo_url,
+            age: result[0].age,
+            taille: result[0].taille,
+            poids: result[0].poids
+          });
+        } else {
+          res.send({
+            error: "Aucun utilisateur trouvé avec cet email."
+          });
+        }
+      } else {
+        res.send({
+          error: "Une erreur s'est produite lors de la récupération des informations de l'utilisateur."
+        });
+      }
+    });
+  });
+});
 
-function checkTokenA(req, res, next) {
-  const expirationTime = 5 * 24 * 60 * 60; // 5 jours en secondes
-  const tokenU = jwt.sign({}, process.env.TOKEN_KEY, { expiresIn: expirationTime });
-  res.header('authorization', tokenU)
-  const bearerHeader = req.headers['authorization'];
-  if (!bearerHeader) {
-    console.log("JWT n'existe pas ");
-    return res.status(403).json({ message: "A token is required for authentication" });
-  }
-  const token = bearerHeader.split(' ')[1];
-  try {
-    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
-    console.log('JWT est valide:', decoded);
-    next();
-  } catch (err) {
-    console.log('JWT est invalide:', err);
-    return res.status(401).json({ message: "Invalid token" });
-  }
-}
-//#endregion
+app.post(url + '/utilisateurs', async (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err
+
+    console.log('Connecté en tant que id ' + connection.threadId)
+    let prenom = req.body.given_name;
+    let nom = req.body.family_name;
+    let email = req.body.email;
+    let photo_url = req.body.picture;
+    let utilisateur = [email, nom, prenom, photo_url];
+
+    let sqlSelect = "SELECT * FROM utilisateurs WHERE email = ?";
+    connection.query(sqlSelect, [email], (err, result) => {
+      if(result.length > 0){
+        connection.release()
+        res.send({
+          message: "L'utilisateur est déjà inscrit"
+        });
+      }else{
+        let sql = "INSERT INTO utilisateurs (email, nom, prenom, photo_url) VALUES ?";
+        connection.query(sql, [[utilisateur]], (err, result) => {
+          connection.release() // return the connection to pool
+          if (!err) {
+            res.send({
+              message: "Utilisateur ajouté avec succès!"
+            });
+          } else {
+            console.log(err)
+            res.send({
+              error: "Une erreur s'est produite lors de l'ajout de l'utilisateur."
+            });
+          }
+        })
+      }
+    })
+  })
+})
+
+app.put(url + '/utilisateurs/:email', async (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err
+    console.log('Connecté en tant que id ' + connection.threadId)
+    let prenom = req.body.given_name;
+    let nom = req.body.family_name;
+    let email = req.params.email;
+    let photo_url = req.body.picture;
+    let age = req.body.age;
+    let taille = req.body.taille;
+    let poids = req.body.poids;
+    let utilisateur = [nom, prenom, photo_url, age, taille, poids, email];
+    let sql = "UPDATE utilisateurs SET nom = ?, prenom = ?, photo_url = ?, age = ?, taille = ?, poids = ? WHERE email = ?";
+    connection.query(sql, utilisateur, (err, result) => {
+      connection.release() // return the connection to pool
+      if (!err) {
+        res.send({
+          message: "Utilisateur modifié avec succès!"
+        });
+      } else {
+        console.log(err)
+        res.send({
+          error: "Une erreur s'est produite lors de la modification de l'utilisateur."
+        });
+      }
+    })
+  })
+})
+
+app.get(url + '/ingredients', async (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err
+    console.log('connected as id ' + connection.threadId)
+    connection.query('SELECT * from ingredients', (err, rows) => {
+      connection.release() // return the connection to pool
+
+      if (!err) {
+        res.send(rows)
+      } else {
+        console.log(err)
+      }
+
+      // if(err) throw err
+      console.log('Données utilisateurs : \n', rows)
+    })
+  })
+})
+
+app.put(url + '/ingredients/:id', async (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err
+    console.log('Connected as id ' + connection.threadId)
+    connection.query(`UPDATE ingredients SET Est_Disponible = ${req.body.Est_Disponible} WHERE ID_Ingredient = ${req.params.id}`, (err, rows) => {
+      connection.release() // return the connection to pool
+
+      if (!err) {
+        res.send(rows)
+      } else {
+        console.log(err)
+      }
+
+      console.log('Data updated : \n', rows)
+    })
+  })
+})
+
+app.get(url + '/cocktails', async (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err
+    console.log('connected as id ' + connection.threadId)
+    connection.query('SELECT * from cocktail WHERE Est_Disponible = 1', (err, rows) => {
+      connection.release() // return the connection to pool
+
+      if (!err) {
+        res.send(rows)
+      } else {
+        console.log(err)
+      }
+
+      // if(err) throw err
+      console.log('Données utilisateurs : \n', rows)
+    })
+  })
+})
+
+app.put(url + '/cocktail/:id', async (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err
+    console.log('Connected as id ' + connection.threadId)
+    connection.query(`UPDATE cocktail SET Est_Disponible = ${req.body.Est_Disponible} WHERE Cocktail_ID = ${req.params.id}`, (err, rows) => {
+      connection.release() // return the connection to pool
+
+      if (!err) {
+        res.send(rows)
+      } else {
+        console.log(err)
+      }
+
+      console.log('Data updated : \n', rows)
+    })
+  })
+})
+
+app.get(url + '/cocktailcomposition', async (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err
+    console.log('connected as id ' + connection.threadId)
+    connection.query(`SELECT * from cocktail_composition`, (err, rows) => {
+      connection.release() // return the connection to pool
+
+      if (!err) {
+        res.send(rows)
+      } else {
+        console.log(err)
+      }
+
+      // if(err) throw err
+      console.log('Données utilisateurs : \n', rows)
+    })
+  })
+})
+
+app.get(url + '/cocktailcomposition/:id', async (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err
+    console.log('connected as id ' + connection.threadId)
+    connection.query(`SELECT * from cocktail_composition WHERE ID_Cocktail = ${req.params.id}`, (err, rows) => {
+      connection.release() // return the connection to pool
+
+      if (!err) {
+        res.send(rows)
+      } else {
+        console.log(err)
+      }
+
+      // if(err) throw err
+      console.log('Données utilisateurs : \n', rows)
+    })
+  })
+})
 
 module.exports = app;
